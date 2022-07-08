3f0735a26ecab2de1cbe26bd7bc1479ee654ac6d
Blake Printy <blakehprinty@gmail.com>
2022-07-08 13:18:31 -0400

    Pages link together

--- src/main/java/com/survivingcodingbootcamp/blog/Populator.java
+++ src/main/java/com/survivingcodingbootcamp/blog/Populator.java
@@ -1,7 +1,9 @@
 package com.survivingcodingbootcamp.blog;
 
+import com.survivingcodingbootcamp.blog.model.Hashtag;
 import com.survivingcodingbootcamp.blog.model.Post;
 import com.survivingcodingbootcamp.blog.model.Topic;
+import com.survivingcodingbootcamp.blog.repository.HashtagRepository;
 import com.survivingcodingbootcamp.blog.repository.PostRepository;
 import com.survivingcodingbootcamp.blog.repository.TopicRepository;
 import org.springframework.boot.CommandLineRunner;
@@ -12,12 +14,14 @@ public class Populator implements CommandLineRunner {
 
     private TopicRepository topicRepo;
     private PostRepository postRepo;
+    private HashtagRepository hashtagRepo;
 
 
-    public Populator(TopicRepository topicRepo, PostRepository postRepo) {
+    public Populator(TopicRepository topicRepo, PostRepository postRepo, HashtagRepository hashtagRepo) {
 
         this.topicRepo = topicRepo;
         this.postRepo = postRepo;
+        this.hashtagRepo = hashtagRepo;
     }
 
     @Override
@@ -26,21 +30,21 @@ public class Populator implements CommandLineRunner {
         Topic topic1 = new Topic("Learning TDD");
         topicRepo.save(topic1);
 
-        Post post1 = new Post("TDD For Fun and Profit", topic1, "Lorem ipsum dolor sit amet, consectetur " +
+        Post post1 = new Post("TDD For Fun and Profit", "Blake", topic1, "Lorem ipsum dolor sit amet, consectetur " +
                 "adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim" +
                 " veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis " +
                 "aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. " +
                 "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id " +
                 "est laborum.");
         postRepo.save(post1);
-        Post post2 = new Post("Test the Fear Away", topic1, "Lorem ipsum dolor sit amet, consectetur " +
+        Post post2 = new Post("Test the Fear Away", "Blake", topic1, "Lorem ipsum dolor sit amet, consectetur " +
                 "adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim" +
                 " veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis " +
                 "aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. " +
                 "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id " +
                 "est laborum.");
         postRepo.save(post2);
-        Post post3 = new Post("Unit Tests and You", topic1, "Lorem ipsum dolor sit amet, consectetur " +
+        Post post3 = new Post("Unit Tests and You", "Blake", topic1, "Lorem ipsum dolor sit amet, consectetur " +
                 "adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim" +
                 " veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis " +
                 "aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. " +
@@ -53,6 +57,9 @@ public class Populator implements CommandLineRunner {
         topicRepo.save(topic3);
         Topic topic4 = new Topic("Object Oriented Programming and You");
         topicRepo.save(topic4);
+
+        Hashtag hashtag1 = new Hashtag("#FirstHashtag");
+        hashtagRepo.save(hashtag1);
 
     }
 
